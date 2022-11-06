420fc905c3df2de63e4f9231a4d62caf6d19f020
kosenda <kkkk0630@ezweb.ne.jp>
2022-11-06 18:59:53 +0900

    implemented cooking_records_header

--- feature/src/main/java/com/oishikenko/android/recruitment/feature/list/RecipeListScreen.kt
+++ feature/src/main/java/com/oishikenko/android/recruitment/feature/list/RecipeListScreen.kt
@@ -1,9 +1,9 @@
 package com.oishikenko.android.recruitment.feature.list
 
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.consumedWindowInsets
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
@@ -12,9 +12,13 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -27,11 +31,31 @@ fun RecipeListScreen(
 ) {
     val cookingRecords by viewModel.cookingRecords.collectAsStateWithLifecycle()
     Scaffold(
+        backgroundColor = MaterialTheme.colors.background,
         topBar = {
             TopAppBar(
                 modifier = Modifier,
+                backgroundColor = MaterialTheme.colors.background,
+                elevation = 0.dp
             ) {
-                Text(text = stringResource(id = R.string.cooking_records_title))
+                Row(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        modifier = Modifier.padding(end = 8.dp),
+                        text = stringResource(id = R.string.cooking_records_title),
+                        color = MaterialTheme.colors.onSurface,
+                        style = MaterialTheme.typography.h6,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Image(
+                        modifier = Modifier.size(64.dp),
+                        painter = painterResource(id = R.drawable.my_cooking_record_header),
+                        contentDescription = null
+                    )
+                }
             }
         }
     ) { innerPadding ->
@@ -48,9 +72,18 @@ fun RecipeListScreen(
     }
 }
 
-@Preview
+@Preview(uiMode = UI_MODE_NIGHT_YES)
 @Composable
-fun PreviewRecipeListScreen(){
+fun PreviewRecipeListScreenDark(){
+    MaterialTheme {
+        RecipeListScreen()
+    }
+}
+
+
+@Preview(uiMode = UI_MODE_NIGHT_NO)
+@Composable
+fun PreviewRecipeListScreenLight(){
     MaterialTheme {
         RecipeListScreen()
     }
